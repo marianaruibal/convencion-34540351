e5c9885573e038744070cdc6ce7e9f0e35753005
marianaruibal <89877848+marianaruibal@users.noreply.github.com>
2021-10-18 23:06:35 -0300

    pruebas faker user

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -27,8 +27,12 @@ class UserFactory extends Factory
         return [
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
+            'title_job' => $this->faker->text,
+            'tel' => '+54114569863',
+            'address' => $this->faker->address,
+            'excerpt' => $this->faker->text,
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => '$2a$12$UDauu/qK4xtGdcs8W.lK0ugkmROMkkR/rbzEEYdGqmDdLH6OrCeuKZ', // password
             'remember_token' => Str::random(10),
         ];
     }
